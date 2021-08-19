e02c7c58f3f4764e4fd24d7b717cf9123fdfd193
chittaranjan2021 <83960157+chittaranjan2021@users.noreply.github.com>
2021-08-19 13:06:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
+echo "Cloning Repo, Please Wait..."
+git clone https://github.com/chittaranjan2021/Youtube-Downloader-Bot /Youtube-Downloader-Bot
+cd /Youtube-Downloader-Bot
+echo "Starting Bot, Please Wait..."
 python3 -m  bot
