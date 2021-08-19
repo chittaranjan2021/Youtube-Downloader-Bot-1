4033cfb49eeb4cb1a372216e131e880ca8670291
chittaranjan2021 <83960157+chittaranjan2021@users.noreply.github.com>
2021-08-19 13:38:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/chittaranjan2021/Youtube-Downloader-Bot.git /Youtube-Downloader-Bot
+git clone https://github.com/chittaranjan2021/Youtube-Downloader-Bot.git
 cd /Youtube-Downloader-Bot
 /Youtube-Downloader-Bot
 pip3 install -U -r requirements.txt
